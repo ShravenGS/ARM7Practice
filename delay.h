9998789a43a6c648b0973ebada0a39d0593d74da
Shraven GS <gsshraven@gmail.com>
2025-07-08 07:50:39 +0530

    Update delay.h

--- delay.h
+++ delay.h
@@ -3,21 +3,21 @@ typedef unsigned int U32;
 delay_second(unsigned int s){
 T0PR=15000000-1;
 T0TCR=0x01;
-while(T0PC<s);
+while(T0TC<s);
 T0TCR=0x03;
 T0TCR=0x01;
 }
 delay_millisecond(U32 ms){
 T0PR=15000-1;
 T0TCR=0x01;
-while(T0PC<ms);
+while(T0TC<ms);
 T0TCR=0x03;
 T0TCR=0x01;
 }
 delay_microsecond(U32 us){
 T0PR=15-1;
 T0TCR=0x01;
-while(T0PC<us);
+while(T0TC<us);
 T0TCR=0x03;
 T0TCR=0x01;
 }

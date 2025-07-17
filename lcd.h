946193b2606d259991f58223ea91e3344c88cc0a
Shraven GS <gsshraven@gmail.com>
2025-07-17 20:03:38 +0530

    Create lcd.h

---
+++ lcd.h
@@ -0,0 +1,130 @@
+/*Before creating custom symbols uncomment that CGRAM function and then try to implement*/
+/*And this is for 4 bit LCD(D4-D7 -LCD)*/
+/*In the main function make sure that this file should be added in your directory*/
+#include<LPC21XX.h>
+#define LCD_D 0xF<<20       
+#define RS    1<<17
+#define RW    1<<18          
+#define E     1<<19
+
+void LCD_INIT(void);
+void LCD_COMMAND(unsigned char);
+void LCD_DATA(unsigned char);
+void LCD_STR(unsigned char []);
+void LCD_SCROLL(unsigned char []);
+void LCD_DECIMAL(float);
+void CGRAM_WRITE(unsigned char);
+int len(char []);
+void delay_ms(int);
+
+void delay_ms(int ms)
+{
+T0PR=15000-1	 ;
+T0TCR=0X01;
+while(T0TC<ms);
+T0TCR=0X03;	 
+T0TCR=0X00;
+}
+
+void LCD_INIT() {
+    IODIR1 |= RS | E | LCD_D |RW;            
+	IOCLR1=RW;
+    LCD_COMMAND(0x02);                    
+    LCD_COMMAND(0x28);                    
+    LCD_COMMAND(0x0C);                    
+    LCD_COMMAND(0x80);                    
+    LCD_COMMAND(0x01);                   
+}
+void LCD_COMMAND(unsigned char cmd)
+ {
+    IOCLR1 = LCD_D;
+	IOSET1= ((cmd&0xF0)<<16);
+	IOCLR1=RS;
+	IOSET1=E;                          
+    delay_ms(2);
+	IOCLR1=E;
+
+	IOCLR1 = LCD_D;
+	IOSET1= ((cmd&0x0F)<<20);
+	IOCLR1=RS;
+	IOSET1=E;                          
+    delay_ms(2);
+	IOCLR1=E;
+}
+
+void LCD_DATA(unsigned char data)
+{
+    IOCLR1 = LCD_D;
+	IOSET1= ((data&0xf0)<<16);
+	IOSET1=RS;
+	IOSET1=E;                          
+    delay_ms(2);
+	IOCLR1=E;
+
+	IOCLR1 = LCD_D;
+	IOSET1= ((data&0x0f)<<20);
+	IOSET1=RS;
+	IOSET1=E;                          
+    delay_ms(2);
+	IOCLR1=E;
+}
+
+
+void LCD_STR(unsigned char s[]){
+	int i=0;
+	while(s[i]!='\0'){
+		LCD_DATA(s[i]);
+		i++;
+	}
+}
+
+
+int len(char s[]){
+	int i,len=0;
+	for(i=0;s[i]!='\0';i++){
+		  len++;
+	}
+return len;
+}
+
+
+void LCD_SCROLL(unsigned char s[]){
+	int length=len(s),i;
+	LCD_COMMAND(0x01);       
+    delay_ms(2);
+    LCD_COMMAND(0x80);        
+    LCD_STR(s);          
+    delay_ms(1000);       
+
+    for (i=0; i<length+16; i++) {  
+        LCD_COMMAND(0x18);           
+        delay_ms(300);              
+    }
+}
+void LCD_DECIMAL(float n){
+		int integer=(int)n;
+		int fraction=(int)((n-integer)*100);
+		if(n<0){
+			LCD_DATA('-');
+			integer=-integer;
+			fraction=-fraction;
+			}
+			LCD_DATA((integer/10)+48);
+			LCD_DATA((integer%10)+48);
+			LCD_DATA('.');
+			LCD_DATA((fraction/10)+48);
+			LCD_DATA((fraction%10)+48);
+}
+/*
+void CGRAM_WRITE(unsigned char nBytes){
+	unsigned char i;
+	LCD_COMMAND(0x40);
+	for(i=0;i<nBytes;i++){
+		LCD_DATA(cgram_lut[i]);
+	}
+}
+*/
+
+
+
+

3009750cd6a1aae9c6126a3cf17b71185a74f8c3
Shraven GS <gsshraven@gmail.com>
2025-07-21 15:04:50 +0530

    Update lcd_4.h

--- lcd/lcd_4.h
+++ lcd/lcd_4.h
@@ -9,16 +9,6 @@
 #define RW    1<<18          
 #define E     1<<19
 
-void LCD_INIT(void);
-void LCD_COMMAND(unsigned char);
-void LCD_DATA(unsigned char);
-void LCD_STR(unsigned char []);
-void LCD_SCROLL(unsigned char []);
-void LCD_DECIMAL(float);
-void CGRAM_WRITE(unsigned char);
-int len(char []);
-void delay_ms(int);
-
 void delay_ms(int ms)
 {
 T0PR=15000-1	 ;
@@ -34,7 +24,10 @@ void LCD_INIT() {
     LCD_COMMAND(0x02);                    
     LCD_COMMAND(0x28);                    
     LCD_COMMAND(0x0C);                    
-    LCD_COMMAND(0x80);                    
+    LCD_COMMAND(0x80);
+    //LCD_COMMAND(0xC0);
+    //LCD_COMMAND(0x94);//Starting address of line 3 for (20x4 display only)
+    //LCD_COMMAND(0xD4);//Starting address of line 4 for (20x4 display only)
     LCD_COMMAND(0x01);                   
 }
 void LCD_COMMAND(unsigned char cmd)
@@ -43,31 +36,31 @@ void LCD_COMMAND(unsigned char cmd)
 	IOSET1= ((cmd&0xF0)<<16);
 	IOCLR1=RS;
 	IOSET1=E;                          
-    delay_ms(2);
+    	delay_ms(2);
 	IOCLR1=E;
 
 	IOCLR1 = LCD_D;
 	IOSET1= ((cmd&0x0F)<<20);
 	IOCLR1=RS;
 	IOSET1=E;                          
-    delay_ms(2);
+    	delay_ms(2);
 	IOCLR1=E;
 }
 
 void LCD_DATA(unsigned char data)
 {
     IOCLR1 = LCD_D;
-	IOSET1= ((data&0xf0)<<16);
+	IOSET1= ((data&0xf0)<<16);//upper nibble writing
 	IOSET1=RS;
 	IOSET1=E;                          
-    delay_ms(2);
+    	delay_ms(2);
 	IOCLR1=E;
 
 	IOCLR1 = LCD_D;
-	IOSET1= ((data&0x0f)<<20);
+	IOSET1= ((data&0x0f)<<20);//lower nibble writing
 	IOSET1=RS;
 	IOSET1=E;                          
-    delay_ms(2);
+    	delay_ms(2);
 	IOCLR1=E;
 }
 

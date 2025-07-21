596c5f2bc19e0fbbcf2a1a3e9399698d5816877b
Shraven GS <gsshraven@gmail.com>
2025-07-21 15:00:46 +0530

    Update lcd_8.h

--- lcd/lcd_8.h
+++ lcd/lcd_8.h
@@ -8,16 +8,6 @@
 #define RS    1<<8          
 #define E     1<<9
 
-void LCD_INIT(void);
-void LCD_COMMAND(unsigned char);
-void LCD_DATA(unsigned char);
-void LCD_STR(unsigned char []);
-void LCD_SCROLL(unsigned char []);
-void LCD_DECIMAL(float);
-void CGRAM_WRITE(unsigned char);
-int len(char []);
-void delay_ms(int);
-
 void delay_ms(int ms)
 {
 T0PR=15000-1	 ;
@@ -29,31 +19,35 @@ T0TCR=0X00;
 
 void LCD_INIT() {
     IODIR0 = RS | E | LCD_D;
-    LCD_COMMAND(0x02);                    
-    LCD_COMMAND(0x38);                    
-    LCD_COMMAND(0x0C);                    
-    LCD_COMMAND(0x80);                    
+    LCD_COMMAND(0x02);// Function home                    
+    LCD_COMMAND(0x38);//8 bit mode set                    
+    LCD_COMMAND(0x0C);//Display ON cursor OFF                    
+    LCD_COMMAND(0x80);//Starting address of line 1 for (16x2 and 20x4 display)
+    LCD_COMMAND(0xC0);//Starting address of line 2 for (16x2 and 20x4 display)
+    //LCD_COMMAND(0x94);//Starting address of line 3 for (20x4 display only)
+    //LCD_COMMAND(0xD4);//Starting address of line 4 for (20x4 display only)
     LCD_COMMAND(0x01);                   
 }
 void LCD_COMMAND(unsigned char cmd)
  {
     IOCLR0 = LCD_D;
-	IOSET0= (cmd&0xFF);
+	IOSET0= (cmd&0xFF);// whatever commands we are giving will take place an and operation with 0xff as it is an 8 bit
 	IOCLR0=RS;
+	//IOCLR0=Rw;// If it is not grounded then this command should be given
 	IOSET0=E;                          
-    delay_ms(2);
-	IOCLR0=E;
+    	delay_ms(2);// Enable will take place only for 2 ms
+	IOCLR0=E;// If it is cleared then only we can write on LCD Display
 
 	
 }
 
 void LCD_DATA(unsigned char data)
 {
-    IOCLR0 = LCD_D;
+	IOCLR0 = LCD_D;
 	IOSET0= (data&0xFF);
 	IOSET0=RS;
 	IOSET0=E;                          
-    delay_ms(2);
+    	delay_ms(2);
 	IOCLR0=E;
 }
 
@@ -85,7 +79,7 @@ void LCD_SCROLL(unsigned char s[]){
     delay_ms(1000);       
 
     for (i=0; i<length+16; i++) {  
-        LCD_COMMAND(0x18);           
+        LCD_COMMAND(0x18);// Display shift left           
         delay_ms(300);              
     }
 }

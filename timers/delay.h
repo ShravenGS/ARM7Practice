1c30c1cd023b4e86d40548ff1e57fd5afc129e71
Shraven GS <gsshraven@gmail.com>
2025-07-21 15:55:44 +0530

    Update delay.h

--- timers/delay.h
+++ timers/delay.h
@@ -21,3 +21,10 @@ while(T0TC<us);
 T0TCR=0x03;
 T0TCR=0x01;
 }
+
+
+// PR-> Prescale register
+//TCR-> Time controlled register
+//TOTC-> Timer register
+
+//When T0PR overflows (after 15000000 cycles), T0TC becomes 1 → That marks 1 second

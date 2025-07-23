515f3a4693f72cf76d704ee53b85ea961c362218
Shraven GS <gsshraven@gmail.com>
2025-07-23 17:43:19 +0530

    Create fiq.c

---
+++ interrupts/external_interrupt/fiq.c
@@ -0,0 +1,27 @@
+#include<LPC21XX.H>
+#define LED1 1<<2
+#define LED2 1<<3
+
+
+void fiq_isr(void) {
+	EXTINT=0x01;//To clear EINT0 flag
+	IOSET0=LED1;
+	IOCLR0=LED1;	
+}
+int main(){
+unsigned int count =0;
+PINSEL1= 0x1;
+
+IODIR0=LED1;
+
+VICIntSelect=1<<14;;
+EXTMODE=0x01;
+EXTPOLAR=0x00;
+VICIntEnable=1<<14;
+
+while(1){
+count++;
+	}
+}
+
+
